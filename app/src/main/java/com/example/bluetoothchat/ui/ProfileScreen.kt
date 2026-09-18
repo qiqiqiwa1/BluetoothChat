04f1e2df851db6a9d12a512b003cbf164ea4499f
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 07:07:56 +0800

    Update ProfileScreen.kt

--- app/src/main/java/com/example/bluetoothchat/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/bluetoothchat/ui/ProfileScreen.kt
@@ -1,19 +1,109 @@
 package com.example.bluetoothchat.ui
 
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 
 @Composable
 fun ProfileScreen(){
 
 
-    Text(
+    Column(
 
-        text = "我的资料"
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+            .padding(20.dp),
 
-    )
+        horizontalAlignment =
+        Alignment.CenterHorizontally
 
+    ){
+
+
+        Icon(
+
+            imageVector =
+            Icons.Default.Person,
+
+            contentDescription = null,
+
+            modifier =
+            Modifier.size(90.dp)
+
+        )
+
+
+        Spacer(
+
+            Modifier.height(20.dp)
+
+        )
+
+
+        Text(
+
+            text = "未设置昵称",
+
+            style =
+            MaterialTheme.typography.headlineSmall
+
+        )
+
+
+        Spacer(
+
+            Modifier.height(30.dp)
+
+        )
+
+
+        Card(
+
+            modifier =
+            Modifier.fillMaxWidth()
+
+        ){
+
+
+            Column(
+
+                modifier =
+                Modifier.padding(20.dp)
+
+            ){
+
+
+                Text(
+                    "设备名称：Android 手机"
+                )
+
+
+                Spacer(
+                    Modifier.height(10.dp)
+                )
+
+
+                Text(
+                    "蓝牙状态：未连接"
+                )
+
+
+            }
+
+
+        }
+
+
+    }
 
 }
