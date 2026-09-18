dee689f60b28a56371befd283c4c69c164b79a82
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 07:07:38 +0800

    Update ContactScreen.kt

--- app/src/main/java/com/example/bluetoothchat/ui/ContactScreen.kt
+++ app/src/main/java/com/example/bluetoothchat/ui/ContactScreen.kt
@@ -1,19 +1,118 @@
 package com.example.bluetoothchat.ui
 
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.People
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 
 
 @Composable
 fun ContactScreen(){
 
 
-    Text(
+    Column(
 
-        text = "联系人列表"
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .safeDrawingPadding()
+            .padding(20.dp)
 
-    )
+    ){
 
+
+        Text(
+
+            text = "联系人",
+
+            style =
+            MaterialTheme.typography.headlineMedium
+
+        )
+
+
+        Spacer(
+
+            Modifier.height(24.dp)
+
+        )
+
+
+        Card(
+
+            modifier =
+            Modifier.fillMaxWidth()
+
+        ){
+
+
+            Column(
+
+                modifier =
+                Modifier.padding(24.dp),
+
+                horizontalAlignment =
+                Alignment.CenterHorizontally
+
+            ){
+
+
+                Icon(
+
+                    imageVector =
+                    Icons.Default.People,
+
+                    contentDescription = null,
+
+                    modifier =
+                    Modifier.size(60.dp)
+
+                )
+
+
+                Spacer(
+
+                    Modifier.height(16.dp)
+
+                )
+
+
+                Text(
+
+                    text = "暂无联系人",
+
+                    style =
+                    MaterialTheme.typography.titleMedium
+
+                )
+
+
+                Spacer(
+
+                    Modifier.height(8.dp)
+
+                )
+
+
+                Text(
+
+                    text = "搜索附近蓝牙设备"
+
+                )
+
+
+            }
+
+
+        }
+
+
+    }
 
 }
