4bbead595180d736af082ec759b2fe42a0567bb4
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 07:04:26 +0800

    Create Color.kt

---
+++ app/src/main/java/com/example/bluetoothchat/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.bluetoothchat.theme
+
+
+import androidx.compose.ui.graphics.Color
+
+
+val BluePrimary = Color(0xFF4A90E2)
+
+val BlueDark = Color(0xFF1565C0)
+
+val BackgroundLight = Color(0xFFF7F7F7)
+
+val BackgroundDark = Color(0xFF121212)
