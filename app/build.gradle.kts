e5ca7a98ffc3dfd345ba516083241d6f1071878f
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 06:01:46 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,16 +16,32 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures { compose = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+
     implementation("androidx.activity:activity-compose:1.9.3")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
     implementation("androidx.core:core-ktx:1.15.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
