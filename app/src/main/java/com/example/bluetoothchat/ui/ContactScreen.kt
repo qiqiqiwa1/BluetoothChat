776439b3a990f5c11bdee2ec5389a5ffc5500eae
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 06:31:00 +0800

    Create ContactScreen.kt

---
+++ app/src/main/java/com/example/bluetoothchat/ui/ContactScreen.kt
@@ -0,0 +1,19 @@
+package com.example.bluetoothchat.ui
+
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ContactScreen(){
+
+
+    Text(
+
+        text = "联系人列表"
+
+    )
+
+
+}
