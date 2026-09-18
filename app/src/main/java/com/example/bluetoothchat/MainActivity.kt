56bfde66b42996f12ac9f8330b94f789138ae425
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 06:29:53 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/bluetoothchat/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothchat/MainActivity.kt
@@ -4,30 +4,156 @@ package com.example.bluetoothchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.*
+import com.example.bluetoothchat.ui.ContactScreen
+import com.example.bluetoothchat.ui.HomeScreen
+import com.example.bluetoothchat.ui.ProfileScreen
 
 
 class MainActivity :
-    ComponentActivity(){
+    ComponentActivity() {
 
 
     override fun onCreate(
         savedInstanceState: Bundle?
-    ){
+    ) {
 
         super.onCreate(savedInstanceState)
 
 
         setContent {
 
-
             MaterialTheme {
 
+                BluetoothChatApp()
 
-                ChatScreen()
+            }
+
+        }
+
+    }
+
+}
+
+
+
+@Composable
+fun BluetoothChatApp() {
+
+
+    val navController =
+        rememberNavController()
+
+
+    Scaffold(
+
+        bottomBar = {
+
+
+            NavigationBar {
+
+
+                NavigationBarItem(
+
+                    selected = false,
+
+                    onClick = {
+
+                        navController.navigate("home")
+
+                    },
+
+                    icon = {},
+
+                    label = {
+
+                        Text("首页")
+
+                    }
+
+                )
+
+
+                NavigationBarItem(
+
+                    selected = false,
+
+                    onClick = {
+
+                        navController.navigate("contacts")
+
+                    },
+
+                    icon = {},
+
+                    label = {
+
+                        Text("联系人")
+
+                    }
+
+                )
+
+
+                NavigationBarItem(
+
+                    selected = false,
+
+                    onClick = {
+
+                        navController.navigate("profile")
+
+                    },
+
+                    icon = {},
+
+                    label = {
+
+                        Text("我的")
+
+                    }
+
+                )
 
 
             }
+
+        }
+
+    ) {
+
+
+        NavHost(
+
+            navController = navController,
+
+            startDestination = "home"
+
+        ) {
+
+
+            composable("home") {
+
+                HomeScreen()
+
+            }
+
+
+            composable("contacts") {
+
+                ContactScreen()
+
+            }
+
+
+            composable("profile") {
+
+                ProfileScreen()
+
+            }
+
 
         }
 
