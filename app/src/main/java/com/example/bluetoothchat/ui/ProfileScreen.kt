5f9f4b2564dbd54243a3765f9771062364ae3267
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 06:31:15 +0800

    Create ProfileScreen.kt

---
+++ app/src/main/java/com/example/bluetoothchat/ui/ProfileScreen.kt
@@ -0,0 +1,19 @@
+package com.example.bluetoothchat.ui
+
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ProfileScreen(){
+
+
+    Text(
+
+        text = "我的资料"
+
+    )
+
+
+}
