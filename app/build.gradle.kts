ef055b03ebce68e71a2bf384f0d1a6c4065927ef
qiqiqiwa1 <119848255+qiqiqiwa1@users.noreply.github.com>
2026-09-19 07:10:23 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,16 +38,28 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.9.3")
 
+
     implementation("androidx.compose.ui:ui")
+
     implementation("androidx.compose.ui:ui-tooling-preview")
 
+
     implementation("androidx.compose.material3:material3")
+
+
+    // Material 图标库
+    implementation(
+        "androidx.compose.material:material-icons-extended"
+    )
+
 
     implementation(
         "androidx.navigation:navigation-compose:2.8.3"
     )
 
+
     implementation("androidx.core:core-ktx:1.15.0")
+
 
     debugImplementation(
         "androidx.compose.ui:ui-tooling"
